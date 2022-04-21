d263e8c7f10214e32f46991c8e356ad22dc70904
renhz2017 <blestmilk@hotmail.com>
2022-04-21 16:45:04 +0800

    feat: 使用mui的layout components實作一些基本的排版

--- src/pages/Test0.tsx
+++ src/pages/Test0.tsx
@@ -1,8 +1,25 @@
-﻿import { Button } from '@mui/material';
+﻿import { Box, Button, Grid, Stack } from '@mui/material';
 import React from 'react';
 
+import MyPaper from 'components/items/myItems';
+
 function Test0() {
-  return <Button variant="outlined">TEST0</Button>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Stack direction="row" spacing={2}>
+            <Button>AAA</Button>
+            <Button variant="outlined">BBB</Button>
+            <Button variant="contained">CCC</Button>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <MyPaper sx={{ width: '100%', height: 300 }}>Result:</MyPaper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default Test0;
--- src/pages/Test1.tsx
+++ src/pages/Test1.tsx
@@ -1,15 +1,34 @@
-﻿import Autocomplete from '@mui/material/Autocomplete';
+﻿import { Box, Grid, Stack } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import React from 'react';
 
+import MyPaper from 'components/items/myItems';
+
 function Test1() {
   return (
-    <Autocomplete
-      id="inputDemo"
-      freeSolo
-      options={['AAA', 'BBB', 'CCC']}
-      renderInput={(params) => <TextField variant="outlined" {...params} label="inputDemo" />}
-    />
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Stack direction="row" spacing={2}>
+            <Autocomplete
+              sx={{ width: 300 }}
+              id="inputDemo"
+              freeSolo
+              options={['AAA', 'BBB', 'CCC']}
+              renderInput={(params) => (
+                <TextField variant="outlined" {...params} label="inputDemo" />
+              )}
+            />
+            <MyPaper sx={{ width: 200 }}>MAX</MyPaper>
+            <MyPaper sx={{ width: 100 }}>WIN</MyPaper>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <MyPaper sx={{ width: '100%', height: 300 }}>Result:</MyPaper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
