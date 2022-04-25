f9e25cdf84b251ac0ede35477eb9baf5461b4bcf
renhz2017 <blestmilk@hotmail.com>
2022-04-25 18:02:28 +0800

    fix: getByRole的設定項name格式, 修正為正規表達式才能部分match

--- src/__test__/test0.test.tsx
+++ src/__test__/test0.test.tsx
@@ -1,4 +1,4 @@
-﻿import { fireEvent, render, screen, within } from '@testing-library/react';
+﻿import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 
@@ -8,7 +8,8 @@ describe('點選按鈕', () => {
   test('點選+1按鈕', () => {
     const mockFn = jest.fn();
     render(<Counter setCount={mockFn} />);
-    const buttonAddOne = within(screen.getByRole('button')).getByText('+1');
+    // const buttonAddOne = screen.getByText('+1', { exact: false });
+    const buttonAddOne = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
     fireEvent.click(buttonAddOne);
     expect(mockFn.mock.calls.length).toBe(1);
   });
