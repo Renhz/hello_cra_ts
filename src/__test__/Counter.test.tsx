a541e7ec3028d8f60d0102341ff2978cc04c2979
renhz2017 <blestmilk@hotmail.com>
2022-04-26 10:45:26 +0800

    fix: 修正部分錯誤程式碼, 也簡化並增加可讀性及彈性

--- src/__test__/Counter.test.tsx
+++ src/__test__/Counter.test.tsx
@@ -1,46 +1,42 @@
-﻿import { fireEvent, render, screen } from '@testing-library/react';
+﻿import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 
 import Counter from '~/components/Counter';
 
 describe('+1按鈕', () => {
+  cleanup();
+  const mockFn = jest.fn();
+  render(<Counter setCount={mockFn} />);
+  const btnAddOne = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
+  fireEvent.click(btnAddOne);
+  const { calls } = mockFn.mock;
   test('onClick事件綁定setCount', () => {
-    const mockFn = jest.fn();
-    render(<Counter setCount={mockFn} />);
-    const buttonAddOne = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
-    fireEvent.click(buttonAddOne);
-    expect(mockFn.mock.calls.length).toBe(1);
+    expect(calls.length).toBe(1);
   });
-  test('點擊+1按鈕, setCount收到的函式正確', () => {
-    const mockFn = jest.fn();
-    render(<Counter setCount={mockFn} />);
-    const buttonAddOne = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
-    fireEvent.click(buttonAddOne);
-    expect(mockFn.mock.calls[0][0](0)).toBe(1);
+  test('點擊+1按鈕, setCount收到的函式邏輯正確', () => {
+    expect(calls[0][0](0)).toBe(1);
   });
 });
 
 describe('reset按鈕', () => {
+  cleanup();
+  const mockFn = jest.fn();
+  render(<Counter setCount={mockFn} />);
+  const btnReset = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
+  fireEvent.click(btnReset);
+  const { calls } = mockFn.mock;
   test('onClick事件綁定setCount', () => {
-    const mockFn = jest.fn();
-    render(<Counter setCount={mockFn} />);
-    const buttonReset = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
-    fireEvent.click(buttonReset);
-    expect(mockFn.mock.calls.length).toBe(1);
+    expect(calls.length).toBe(1);
   });
-  test('點擊reset按鈕, 無initialCount值, setCount收到0', () => {
-    const mockFn = jest.fn();
-    render(<Counter setCount={mockFn} />);
-    const buttonReset = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
-    fireEvent.click(buttonReset);
-    expect(mockFn.mock.calls[0][0]).toBe(0);
+  test('點擊reset按鈕, setCount收到0', () => {
+    expect(calls[0][0]).toBe(0);
   });
-  test('點擊reset按鈕, 有initialCount值, setCount收到initialCount', () => {
-    const mockFn = jest.fn();
+  test('如有initialCount值, setCount收到initialCount', () => {
     render(<Counter setCount={mockFn} initialCount={10} />);
-    const buttonReset = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
-    fireEvent.click(buttonReset);
-    expect(mockFn.mock.calls[0][0]).toBe(10);
+    const btnResetWithInit = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
+    fireEvent.click(btnResetWithInit);
+    const callsWithInit = mockFn.mock.calls;
+    expect(callsWithInit[0][0]).toBe(10);
   });
 });
