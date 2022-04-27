c1422aaee957ffc5f71a63fe39220ae29ad958d5
Renhz <blestmilk@hotmail.com>
2022-04-27 23:43:25 +0800

    feat: inputPassword新增於onBlur時驗證密碼功能

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,10 +1,35 @@
 ﻿import TextField from '@mui/material/TextField';
 import React, { ChangeEvent, useState } from 'react';
 
+const validatePassword = (inputValue: string) => {
+  const result = [];
+  if (inputValue.length <= 8) {
+    result.push('長度須大於8個字');
+  }
+  if (!/[A-Z]+/.test(inputValue)) {
+    result.push('須包含至少1個大寫英文字');
+  }
+  if (!/[0-9]+/.test(inputValue)) {
+    result.push('須包含至少1個數字');
+  }
+  if (result.length > 0) {
+    throw new Error(result.join(', '));
+  }
+};
+
 const InputPassword = function InputPassword() {
   const [inputValue, setInputValue] = useState('');
+  const [helptext, setHelpText] = useState('');
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInputValue(e.target.value);
+  };
+  const handleBlur = () => {
+    try {
+      validatePassword(inputValue);
+      setHelpText('');
+    } catch (e: any) {
+      setHelpText(e.message);
+    }
   };
   return (
     <TextField
@@ -14,14 +39,11 @@ const InputPassword = function InputPassword() {
       sx={{ width: 300 }}
       value={inputValue}
       onChange={handleChange}
+      onBlur={handleBlur}
+      error={!!helptext}
+      helperText={helptext}
     />
   );
 };
 
 export default InputPassword;
-
-/*
-
-    
-
-*/
