e15eb87667a8a3ef49bf6b129365972932f2e12e
renhz2017 <blestmilk@hotmail.com>
2022-04-27 17:16:30 +0800

    test: 建置虛擬的ThemeProvider環境(未完成)

---
+++ src/__test__/myTestUtils.tsx
@@ -0,0 +1,24 @@
+﻿import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { render, RenderOptions } from '@testing-library/react';
+import * as React from 'react';
+
+interface MyThemeProviderProps {
+  children: React.ReactNode;
+}
+
+function MyThemeProvider({ children }: MyThemeProviderProps) {
+  const myDefault: { [index: string]: any } = {
+    theme: createTheme({
+      palette: {
+        mode: 'light',
+      },
+    }),
+  };
+  return <ThemeProvider theme={myDefault.theme}>{children}</ThemeProvider>;
+}
+
+const renderWithMyTheme = (ui: React.ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: MyThemeProvider, ...options });
+
+export * from '@testing-library/react';
+export { renderWithMyTheme };
