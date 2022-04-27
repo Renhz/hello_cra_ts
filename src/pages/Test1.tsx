ab79bd25f6a29c28c0774a52ed93c72639d7f9bb
renhz2017 <blestmilk@hotmail.com>
2022-04-27 17:18:48 +0800

    refactor: 重構inputAccount為Login元件(未完成)

--- src/pages/Test1.tsx
+++ src/pages/Test1.tsx
@@ -1,10 +1,11 @@
-﻿import { Box, Grid, Stack } from '@mui/material';
-import React from 'react';
+﻿import { Box, Grid, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
-import InputPassword from 'components/InputAccount';
+import InputPassword from 'components/LogIn';
 import { MyPaper } from 'components/myStyled';
 
 function Test1() {
+  const [result] = useState('');
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
@@ -14,7 +15,12 @@ function Test1() {
           </Stack>
         </Grid>
         <Grid item xs={4}>
-          <MyPaper sx={{ width: '100%', height: 300 }}>Result:</MyPaper>
+          <MyPaper sx={{ width: '100%', height: 300 }}>
+            <Typography variant="h4">Result:</Typography>
+            <Typography align="center" variant="h6" sx={{ fontSize: '5em' }}>
+              {result}
+            </Typography>
+          </MyPaper>
         </Grid>
       </Grid>
     </Box>
