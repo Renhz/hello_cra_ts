4d29bd9a5110364c5a6e5633ea286f530326a3a6
renhz2017 <blestmilk@hotmail.com>
2022-04-28 11:44:25 +0800

    test: 測試名稱及變數名稱調整, 增加易讀性

--- src/__test__/Counter.test.tsx
+++ src/__test__/Counter.test.tsx
@@ -6,37 +6,37 @@ import Counter from '~/components/Counter';
 
 describe('+1按鈕', () => {
   cleanup();
-  const mockFn = jest.fn();
-  render(<Counter setCount={mockFn} />);
+  const mockSetCount = jest.fn();
+  render(<Counter setCount={mockSetCount} />);
   const btnAddOne = screen.getByRole('button', { exact: false, name: /\b(count|\+1)/ });
   fireEvent.click(btnAddOne);
-  const { calls } = mockFn.mock;
-  test('onClick事件綁定setCount', () => {
+  const { calls } = mockSetCount.mock;
+  test('setCount函式成功綁定於按鈕', () => {
     expect(calls.length).toBe(1);
   });
-  test('點擊+1按鈕, setCount收到的函式邏輯正確', () => {
+  test('setCount收到的update函式邏輯正確 0=>0+1', () => {
     expect(calls[0][0](0)).toBe(1);
   });
 });
 
 describe('reset按鈕', () => {
   cleanup();
-  const mockFn = jest.fn();
-  render(<Counter setCount={mockFn} />);
+  const mockSetCount = jest.fn();
+  render(<Counter setCount={mockSetCount} />);
   const btnReset = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
   fireEvent.click(btnReset);
-  const { calls } = mockFn.mock;
-  test('onClick事件綁定setCount', () => {
+  const { calls } = mockSetCount.mock;
+  test('setCount函式成功綁定於按鈕', () => {
     expect(calls.length).toBe(1);
   });
-  test('點擊reset按鈕, setCount收到0', () => {
+  test('setCount收到的參數為0, 重置為0', () => {
     expect(calls[0][0]).toBe(0);
   });
-  test('如有initialCount值, setCount收到initialCount', () => {
-    render(<Counter setCount={mockFn} initialCount={10} />);
+  test('如有設定initialCount值為10, 重置為10', () => {
+    render(<Counter setCount={mockSetCount} initialCount={10} />);
     const btnResetWithInit = screen.getByRole('button', { exact: false, name: /\b(reset|\+1)/ });
     fireEvent.click(btnResetWithInit);
-    const callsWithInit = mockFn.mock.calls;
+    const callsWithInit = mockSetCount.mock.calls;
     expect(callsWithInit[0][0]).toBe(10);
   });
 });
