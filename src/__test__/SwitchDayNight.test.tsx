0dc735dc75be08899f55d2ed73e855c20f55829a
renhz2017 <blestmilk@hotmail.com>
2022-04-28 13:21:39 +0800

    fix: mock.calls要在test開始前會被自動清除, 要先記下

--- src/__test__/SwitchDayNight.test.tsx
+++ src/__test__/SwitchDayNight.test.tsx
@@ -1,8 +1,6 @@
 ﻿import * as React from 'react';
-
 import '@testing-library/jest-dom';
 
-// import { SwitchDayNight } from '~/components/myStyled';
 import SwitchDayNight from '~/components/myGeneralUI';
 
 import { renderWithMyProviders, screen, fireEvent } from './myTestUtils';
@@ -17,15 +15,15 @@ describe('使用於DashboardContext環境, 從useContext取得setTheme函式', (
   renderWithMyProviders(<SwitchDayNight />, { providerProps });
   const mySwitch = screen.getByRole('checkbox');
   fireEvent.click(mySwitch);
-  screen.debug(mySwitch);
+  const { calls } = mockSetTheme.mock;
   test('點選switch成功呼叫setTheme', () => {
-    expect(mockSetTheme.mock.calls.length).toBe(1);
+    expect(calls.length).toBe(1);
   });
   test('setTheme收到的update函式邏輯正確', () => {
     const themeInit = {
       palette: { mode: 'light' },
     };
-    const toggle = mockSetTheme.mock.calls[0][0];
+    const toggle = calls[0][0];
     const themeChanged = toggle(themeInit);
     const themeChangedBack = toggle(themeChanged);
     expect(themeChanged.palette.mode).toBe('dark');
