15b5d3f70422c09b97d24b9a910cade5c99b082c
renhz2017 <blestmilk@hotmail.com>
2022-05-03 13:48:49 +0800

    test: 增加測試切換時將設定存入localStorage

--- src/__test__/SwitchColorMode.test.tsx
+++ src/__test__/SwitchColorMode.test.tsx
@@ -8,15 +8,32 @@ import SwitchColorMode from '~/features/theme/SwitchColorMode';
 
 import { screen, fireEvent } from './myTestUtils';
 
+const mockLocalStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+};
+Object.defineProperty(window, 'localStorage', { value: mockLocalStorage });
+
 describe('<SwitchColorMode/> 渲染於redux環境', () => {
-  render(
-    <Provider store={store}>
-      <SwitchColorMode />
-    </Provider>
-  );
-  const mySwitch = screen.getByRole('checkbox');
   test('theme.colorMode預設為light，點選switch後切換為dark', () => {
+    render(
+      <Provider store={store}>
+        <SwitchColorMode />
+      </Provider>
+    );
+    const mySwitch = screen.getByRole('checkbox');
     fireEvent.click(mySwitch);
     expect(store.getState().theme.colorMode).toBe('dark');
+  });
+  test('切換亮暗色時將設定存入localStorage的colorMode', () => {
+    render(
+      <Provider store={store}>
+        <SwitchColorMode />
+      </Provider>
+    );
+    const mySwitch = screen.getByRole('checkbox');
+    fireEvent.click(mySwitch);
+    expect(mockLocalStorage.setItem.mock.calls[0][0]).toBe('colorMode');
+    expect(mockLocalStorage.setItem.mock.calls[0][1]).toBe('dark');
   });
 });
